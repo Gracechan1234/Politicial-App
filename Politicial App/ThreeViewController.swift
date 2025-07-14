22cb1fbabbfb012b4334b04091d246a0adc5e469
Grace Chan <gracechan2028@cdnis.edu.hk>
2025-07-14 17:26:09 +0800

    July 14
    - Started core app

---
+++ Politicial App/ThreeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThreeViewController.swift
+//  Politicial App
+//
+//  Created by grace chan on 14/7/2025.
+//
+
+import UIKit
+
+class ThreeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Politicial App/TwoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TwoViewController.swift
+//  Politicial App
+//
+//  Created by grace chan on 14/7/2025.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    @IBOutlet weak var studySlider: UISlider!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func sliderChanged(_ sender: Any) {
+    }
+    
+}
