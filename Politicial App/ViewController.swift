2667e0b8e1c90ebc90a5dc77fb89ee98f16ab4eb
Grace Chan <gracechan2028@cdnis.edu.hk>
2025-07-14 16:36:15 +0800

    Initial Commit

---
+++ Politicial App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Politicial App
+//
+//  Created by grace chan on 14/7/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Politicial AppTests/Politicial_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Politicial_AppTests.swift
+//  Politicial AppTests
+//
+//  Created by grace chan on 14/7/2025.
+//
+
+import Testing
+@testable import Politicial_App
+
+struct Politicial_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
